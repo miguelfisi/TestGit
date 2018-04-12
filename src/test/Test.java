b0ee4f9e322ef4d4da0750e033e5dc4060b09df8
mpabloto <mpabloto@LIM-JQ8NZM2.usersad.everis.int>
2018-04-12 02:08:35 -0500

    version 01 de programa Test

---
+++ src/test/Test.java
@@ -0,0 +1,14 @@
+package test;
+
+public class Test {
+
+	public static void main(String[] args) {
+		//Test t = new Test();
+		System.out.println(Test.suma(3, 1));
+	}
+
+	public static int suma(int x, int y) {
+		return x + y;
+	}
+
+}
