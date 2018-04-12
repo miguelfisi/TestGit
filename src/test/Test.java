a71b2e9ecf0057d60d8d66fcfdd85943686e88c3
mpabloto <mpabloto@LIM-JQ8NZM2.usersad.everis.int>
2018-04-12 09:56:08 -0500

    version 2 TestGit

--- src/test/Test.java
+++ src/test/Test.java
@@ -3,16 +3,20 @@ package test;
 public class Test {
 
 	public static void main(String[] args) {
-		//Test t = new Test();
+		// Test t = new Test();
 		System.out.println(Test.suma(3, 1));
 	}
 
 	public static int suma(int x, int y) {
 		return x + y;
 	}
-	
-	public static int multiplicacion (int x, int y) {
-		return x*y;
+
+	public static int resta(int x, int y) {
+		return x - y;
+	}
+
+	public static int multiplicacion(int x, int y) {
+		return x * y;
 	}
 
 }
